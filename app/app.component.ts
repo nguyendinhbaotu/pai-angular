ec9ac783a43dfbb884048cef524132a675b0307c
Tu Nguyen <nguyendinhbaotu@gmail.com>
2016-12-18 10:46:08 +0100

    Remove all - Initial with Angular 2 & Webpack

--- app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'pai-angular',
-    template: 'app/app.html'
-})
-export class AppComponent {
-    pageTitle: string = 'First version of PAI';
-}
